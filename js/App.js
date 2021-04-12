e6e0effa405de766e331df2566f10e3574fabf63
duduvalle77 <duduvalle77@yahoo.com.br>
2021-04-12 14:57:24 -0300

    Movimento do mapa, simbolos, e alternar camadas

--- js/App.js
+++ js/App.js
@@ -9,6 +9,9 @@ let x = 100;
 let y = 150;
 let vx = 0;
 let vy = 0;
+let xMapa = 0;
+let yMapa = 0;
+let camada = 6;
 
 const NumberGem = 3;
 const SIDE = 5;
@@ -41,12 +44,15 @@ function criaGerador(indice)
     switch (indice) {
         case 0:
             planets[gr][gc].color = "red";
+            planets[gr][gc].role = 0;
             break;
         case 1:
             planets[gr][gc].color = "blue";
+            planets[gr][gc].role = 1;
             break;
         case 2:
             planets[gr][gc].color = "green";
+            planets[gr][gc].role = 2;
             break;
     }
     
@@ -66,12 +72,15 @@ function criaConsumidor(indice)
     switch (indice) {
         case 0:
             planets[gr][gc].color = "Crimson";
+            planets[gr][gc].role = 3;
             break;
         case 1:
             planets[gr][gc].color = "Cyan";
+            planets[gr][gc].role = 4;
             break;
         case 2:
             planets[gr][gc].color = "SpringGreen";
+            planets[gr][gc].role = 5;
             break;
     }
     maps.addElement(indice + NumberGem, gr, gc);
@@ -92,6 +101,7 @@ let dt;
 requestAnimationFrame(desenha);
 
 function desenha(t) {
+    ctx.translate(xMapa, yMapa);
     t0 = t0 ?? t;
     dt = (t - t0) / 1000;
     //Desenha Fundo
@@ -117,13 +127,24 @@ function desenha(t) {
     ctx.restore();
     requestAnimationFrame(desenha);
     t0 = t;
+    ctx.resetTransform();
 }
 
 function desenhaPlanetas() {
     //Desenha Fundo
+    
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
+    if(camada == 6){
+        ctx.fillStyle = "white";
+        ctx.fillText("Nenhuma camada selecionada", 0, 230);
+    }
+    else
+    {
+        ctx.fillStyle = "white";
+        ctx.fillText("Camada selecionada: " + camada, 0, 230);
+    }
 
     //Desenha Matrix de Planetas
     for (let r = 0; r < planets.length; r++) {
@@ -132,16 +153,70 @@ function desenhaPlanetas() {
             const planet = planets[r][c];
             ctx.fillStyle = planet.color;
             ctx.beginPath();
-            ctx.ellipse(planet.x, planet.y, 7, 7, 0, 0, 2 * Math.PI, false);
+            if(planet.role < 3) {
+                ctx.rect(planet.x, planet.y, 8, 8)
+            } else if(planet.role < 6) {
+                ctx.ellipse(planet.x, planet.y, 4, 7, 0, 0, 2 * Math.PI, false);
+            } else {
+                ctx.ellipse(planet.x, planet.y, 7, 7, 0, 0, 2 * Math.PI, false);
+            }
+            
             ctx.fill();
             ctx.strokeStyle = "grey";
             ctx.stroke();
-            ctx.fillStyle = "red";
-            ctx.fillText(maps.getElement(0, r, c), planet.x + 10, planet.y + 10);
-            ctx.fillStyle = "blue";
-            ctx.fillText(maps.getElement(1, r, c), planet.x + 20, planet.y + 10);
-            ctx.fillStyle = "green";
-            ctx.fillText(maps.getElement(2, r, c), planet.x + 30, planet.y + 10);
+            if(camada == 6){
+                ctx.fillStyle = "red";
+                ctx.fillText(maps.getElement(0, r, c), planet.x + 10, planet.y + 10);
+                ctx.fillStyle = "blue";
+                ctx.fillText(maps.getElement(1, r, c), planet.x + 20, planet.y + 10);
+                ctx.fillStyle = "green";
+                ctx.fillText(maps.getElement(2, r, c), planet.x + 30, planet.y + 10);
+            }
+            else
+            {
+                ctx.fillStyle = "white";
+                ctx.fillText(maps.getElement(camada, r, c), planet.x + 10, planet.y + 10);
+            }
+            
         }
     }
+    
 }
+
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "0":
+            camada = 0;
+            break;
+        case "1":
+            camada = 1;
+            break;
+        case "2":
+            camada = 2;
+            break;
+        case "3":
+            camada = 3;
+            break;
+        case "4":
+            camada = 4;
+            break;
+        case "5":
+            camada = 5;
+            break;
+        case "6":
+            camada = 6;
+            break;
+        case "ArrowLeft":
+            xMapa = xMapa - 10;
+            break;
+        case "ArrowRight":
+            xMapa = xMapa + 10;
+            break;
+        case "ArrowUp":
+            yMapa = yMapa - 10;
+            break;
+        case "ArrowDown":
+            yMapa = yMapa + 10;
+            break;
+    }
+});
--- js/Planet.mjs
+++ js/Planet.mjs
@@ -6,6 +6,7 @@ export default class Planet {
         this.x = this.column*SIZE;
         this.y = this.row*SIZE;
         this.color = "white";
+        this.role = 6;
         /*this.contents = [
             GEM_RED,
             BUYER_GREEN
